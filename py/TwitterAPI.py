111a0f0354b874406cd786a128cc9fca5750ec74
Emily Simoneau <simoneau.e@husky.neu.edu>
2018-08-09 03:10:42 -0400

    Added preliminary date filtering for TwitterAPI. Unsure about since parameter

--- py/TwitterAPI.py
+++ py/TwitterAPI.py
@@ -12,16 +12,29 @@ class TwitterAPI(APICall):
 			self.creds = json.load(file)
 		
 
-	def search(self, keyword, date_start='', date_end=''):
+	def search(self, keyword, date_start, date_end):
 		self.results = {'user': [], 'date': [], 'text': [], 'favorite_count': [], 'user_loc':[]}
 		python_tweets = Twython(self.creds['CONSUMER_KEY'], self.creds['CONSUMER_SECRET'])
-		for status in python_tweets.search(q=keyword)['statuses']:
+		date_start_str
+		date_end_str = self.date_to_string(self, date_end)
+		for status in python_tweets.search(q=keyword, until=date_end_str, since=date_start_str)['statuses']:
+			if(status['created_at'
 			self.results['user'].append(status['user']['screen_name'])
 			self.results['date'].append(status['created_at'])
 			self.results['text'].append(status['text'])
 			self.results['favorite_count'].append(status['favorite_count'])
 			self.results['user_loc'].append(status['user']['location'])
 
+	def date_to_string(self, date) :
+		date_string = str(date.year) + '-'
+		if date.month < 10:
+			date_string += '0'
+		date_string += date.month + '-'
+		if date.day < 10:
+			date_string += '0'
+		date_string += date.day
+		return date_string
+			 
 	def parse_results(self):
 		if self.results != {}:
 			with open('../json/twitterData.json', 'w') as outfile:
